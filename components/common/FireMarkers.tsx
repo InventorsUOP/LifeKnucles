4b4fb570ab19dee2176d31b5a8189ddf85757266
dasunsilva <e19375@eng.pdn.ac.lk>
2024-09-08 22:51:26 +0530

    Removed unused icons

--- components/common/FireMarkers.tsx
+++ components/common/FireMarkers.tsx
@@ -3,8 +3,6 @@ import { Marker } from "react-native-maps";
 import { Icon } from "react-native-paper";
 
 const MapFireIndicator = ({ fireSection }: { fireSection: MapSection }) => {
-  const fireIcon = require("../../assets/images/fire.png");
-
   const { sectionCenter } = fireSection;
   const defaultCoordinate: Coordinate = { latitude: 0, longitude: 0 };
 
