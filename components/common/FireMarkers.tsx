987eaafe07adc168ba05d0c8c71ce34664b5a0d4
dasunsilva <e19375@eng.pdn.ac.lk>
2024-09-09 00:00:01 +0530

    Finihsed feature

---
+++ components/common/FireMarkers.tsx
@@ -0,0 +1,17 @@
+import { Marker } from "react-native-maps";
+import { Icon } from "react-native-paper";
+
+const MapFireIndicator = ({ fireSection }: { fireSection: MapSection }) => {
+  const { sectionCenter } = fireSection;
+  const defaultCoordinate: Coordinate = { latitude: 0, longitude: 0 };
+
+  const centerCoordinate = sectionCenter || defaultCoordinate;
+
+  return (
+    <Marker coordinate={centerCoordinate}>
+      <Icon source={"fire-circle"} size={40} />
+    </Marker>
+  );
+};
+
+export default MapFireIndicator;
