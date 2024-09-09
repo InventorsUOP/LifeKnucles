46472d458d772abf0cf2f9d479721ef8e877ad40
chiran-cyber298 <101457272+chiran-cyber298@users.noreply.github.com>
2024-09-10 01:05:38 +0530

    Update image.ts
    added image imports

--- constants/image.ts
+++ constants/image.ts
@@ -4,8 +4,8 @@ import path from "../assets/images/path.png"
 import bgimage from "../assets/images/bg.webp"
 import wildfire from "../assets/images/wildfire1.jpg"
 import backgroundimage from "../assets/images/background-image.jpeg"
+import knuclkes from "../assets/images/background-image.jpeg"
+import safety from "../assets/images/wildfire2.jpeg"
 
 
-
-
-export default {logo, logobg, path, bgimage,wildfire,backgroundimage};
+export default {logo, logobg, path, bgimage,wildfire,backgroundimage,knuclkes,safety};
