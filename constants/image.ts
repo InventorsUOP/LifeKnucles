0bcbdd1afe21fe3ccfd959511879203f558c9b8e
Kavindu_Methpura <kavindumethpura@gmail.com>
2024-09-14 08:43:45 +0530

    new background

--- constants/image.ts
+++ constants/image.ts
@@ -1,6 +1,6 @@
 import logo from "../assets/images/Logo.jpg";
 import logobg from "../assets/images/Logo-removebg.png";
 import path from "../assets/images/path.png"
-import bgimage from "../assets/images/bg.webp"
+import bgimage from "../assets/images/bg1.webp"
 
 export default {logo, logobg, path, bgimage};
