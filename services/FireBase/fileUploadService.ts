dfc07cd38bb080647d4d4099e8744ad8cf7ee4d6
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2024-10-07 19:37:01 +0530

    fix sonar issue

--- services/FireBase/fileUploadService.ts
+++ services/FireBase/fileUploadService.ts
@@ -1,10 +1,12 @@
-import { storageRef, storage, uploadBytes, getDownloadURL } from "./firebaseConfig";
+import { storageRef, storage, uploadBytes, getDownloadURL, rtdb } from "./firebaseConfig";
+import { ref as databaseRef, push } from "firebase/database";
 
 export const uploadPhoto = async (photo: string[]) => {
     const uploadUrls = [];
 
     for (const image of photo) {
-        const photoRef = storageRef(storage, `fireAlerts/${Date.now()}_${Math.random()}.jpg`);
+        const uniqueKey = push(databaseRef(rtdb, 'uniqueKeys')).key;
+        const photoRef = storageRef(storage, `fireAlerts/${Date.now()}_${uniqueKey}.jpg`);
 
         const response = await fetch(image);
         const blob = await response.blob();
